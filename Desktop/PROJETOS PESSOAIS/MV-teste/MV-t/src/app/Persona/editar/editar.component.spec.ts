8b808e7faf01d579f8a5690ace18f5a05c25ebac
Ezequiel Gomes lopes <60904569+RabinoMachineLearning@users.noreply.github.com>
2021-11-10 17:20:50 -0300

    Update editar.component.spec.ts

--- Desktop/PROJETOS PESSOAIS/MV-teste/MV-t/src/app/Persona/editar/editar.component.spec.ts
+++ Desktop/PROJETOS PESSOAIS/MV-teste/MV-t/src/app/Persona/editar/editar.component.spec.ts
@@ -1,25 +1,21 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
 import { EditarComponent } from './editar.component';
-
 describe('EditarComponent', () => {
-  let component: EditarComponent;
-  let fixture: ComponentFixture<EditarComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ EditarComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(EditarComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    let component;
+    let fixture;
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [EditarComponent]
+        })
+            .compileComponents();
+    });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(EditarComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
+//# sourceMappingURL=editar.component.spec.js.map
