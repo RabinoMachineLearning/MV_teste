9efd08783d2e36a9ec014910bbf456294bb4ab3b
Ezequiel Gomes lopes <60904569+RabinoMachineLearning@users.noreply.github.com>
2021-11-10 17:17:11 -0300

    Update editar.component.ts

--- Desktop/PROJETOS PESSOAIS/MV-teste/MV-t/src/app/Persona/editar/editar.component.ts
+++ Desktop/PROJETOS PESSOAIS/MV-teste/MV-t/src/app/Persona/editar/editar.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'app-editar',
-  templateUrl: './editar.component.html',
-  styleUrls: ['./editar.component.scss']
-})
-export class EditarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+import { __decorate } from "tslib";
+import { Component } from '@angular/core';
+let EditarComponent = class EditarComponent {
+    constructor() { }
+    ngOnInit() {
+    }
+};
+EditarComponent = __decorate([
+    Component({
+        selector: 'app-editar',
+        templateUrl: './editar.component.html',
+        styleUrls: ['./editar.component.scss']
+    })
+], EditarComponent);
+export { EditarComponent };
+//# sourceMappingURL=editar.component.js.map
